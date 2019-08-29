30643d074c37d91c3fbd6cc0393d2a5b6af5c1c5
Ismael <ismaelfilipe@gmail.com>
2019-08-29 07:04:48 -0300

    Improvements on code style.

--- print_matrix/print_matrix.c
+++ print_matrix/print_matrix.c
@@ -9,7 +9,7 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
  *
@@ -36,15 +36,14 @@
 
 void printer(const char * buffer,
              const int len) {
-    fprintf(stdout, "\nPrinting (%d) elements...\n", len);
+    fprintf(stdout, "\n\nPrinting (%d) elements...\n", len);
     for (int i = 0; i < len; i++) {
         fprintf(stdout, "0x%02X ", buffer[i]);
-        //fprintf(stdout, "%c ", buffer[i]);
     }
-    fprintf(stdout, "\nPrinting finished.\n");
+    fprintf(stdout, "\nPrinting finished.\n\n");
 }
 
-int main(int argc, 
+int main(int argc,
          char * argv[]) {
     int lh_count = 1;
     int hm = 1;
@@ -59,53 +58,54 @@ int main(int argc,
         printf("Heigth: (%d)\n", HEIGHTLEN);
 
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 10; j+=HEIGHTLEN) {                                
+            for (int j = 0; j < 10; j+=HEIGHTLEN) {
                 int h = j + HEIGHTLEN;
 
-                if (h > 10) 
+                if (h > 10)
                     h -= h - 10;
 
                 for (; h > j ; h--) {
-                    fprintf(stdout, "i(%d) h-1(%d) j(%d) (%c)\n", i, (h-1), j, hMatrixTester[i][h-1]);
+                    fprintf(stdout, "i(%d) j(%d) h-1(%d) (%c)\n", i, j, (h-1), hMatrixTester[i][h-1]);
                 }
                 fprintf(stdout, "\n");
             }
-            fprintf(stdout, "\n");            
+            fprintf(stdout, "\n");
         }
         return 0;
     }
 
-    memset(line, 0, sizeof(line));    
+    memset(line, 0, sizeof(line));
 
     for (int i = 0; i < ELEMENTS; i++)
         fprintf(stdout, "%s\n", matrix[i]);
 
-    for (int i = 0; i < ELEMENTS; i++) {        
+    printf("Elements: (%d)\nHeight: (%d)\n", ELEMENTS, HEIGHTLEN);
+
+    for (int i = 0; i < ELEMENTS; i++) {
         for (int j = 0; j < STRLEN; j+=HEIGHTLEN) {
             int h = j + HEIGHTLEN;
 
-            if (h > STRLEN) 
+            if (h > STRLEN)
                 h -= h - STRLEN;
 
             for (; h > j ; h--) {
-                //fprintf(stdout, "i(%d) h-1(%d) j(%d) (%c)\n", i, (h-1), j, hMatrixTester[i][h-1]);
                 fprintf(stdout, "matrix[%d][%d] = %c\n", i, h-1, matrix[i][h-1]);
                 if (matrix[i][h-1] == '#') {
-                    fprintf(stdout, "%d %d # line: 0x%02X mult: 0x%02X result: 0x%02X\n",i ,j , line[h], hm, (line[h] | hm));
-                    line[h] = line[h] | hm;                
-                }      
-            }      
+                    fprintf(stdout, "matrix[%d][%d] - line: 0x%02X mult: 0x%02X result: 0x%02X\n",i ,j , line[h], hm, (line[h] | hm));
+                    line[h] = line[h] | hm;
+                }
+            }
         }
 
         printf("\n");
 
         if (lh_count < HEIGHTLEN) {
-            lh_count++;  
-            hm*=2;          
+            lh_count++;
+            hm *= 2;
             continue;
-        }            
-        
-        printer(line, STRLEN);        
+        }
+
+        printer(line, STRLEN);
         lh_count = 1;
         hm = 1;
         memset(line, 0, sizeof(line));
